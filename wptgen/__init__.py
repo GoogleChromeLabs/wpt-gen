bdfbcaecd0df17bde57b2a72525dbaf9e5db28de
suzyliu <suzyliu@google.com>
2026-05-21 21:55:58 +0000

    chore: bump version to v0.6.1 for release

--- wptgen/__init__.py
+++ wptgen/__init__.py
@@ -19,7 +19,7 @@ from __future__ import annotations
 from wptgen.engine import WPTGenEngine
 from wptgen.ui import LoggingUIProvider
 
-__version__ = "0.6.0"
+__version__ = "0.6.1"
 
 
 def generate_audit_report(
