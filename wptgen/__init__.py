1007d127f13efe13628b2e1823734285aa5c87dc
Suzy Liu <36471371+szy196@users.noreply.github.com>
2026-07-29 12:33:43 -0700

    release: bump version to v0.6.4 (#547)

--- wptgen/__init__.py
+++ wptgen/__init__.py
@@ -19,7 +19,7 @@ from __future__ import annotations
 from wptgen.engine import WPTGenEngine
 from wptgen.ui import LoggingUIProvider
 
-__version__ = "0.6.3"
+__version__ = "0.6.4"
 
 
 def generate_audit_report(
